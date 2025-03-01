d6f26714c322218e1fd2fe665d02134d4c17bf03
Samso9th <agbosamsonite@gmail.com>
2025-03-01 03:42:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pip-cleanup",
-    version="0.1.82",
+    version="0.1.83",
     author="Samso9th",
     description="A Python tool to clean up unused pip packages",
     long_description="A CLI tool that helps you identify and remove unused pip packages from your Python environment",
@@ -24,4 +24,5 @@ setup(
         ],
     },
     python_requires=">=3.7",
+    license="MIT",
 )
